5c4a7a0522659c2fd9e7ab7a6e80b369c6b7f1d4
Babak Shahian Jahromi <BabakShah@users.noreply.github.com>
2018-01-15 23:51:26 -0600

    Update the python functions, add comments.

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -2,85 +2,6 @@
 # It will prompt a user with a paragraph containing several blanks.
 # The user should then fill in each blank appropriately to complete the paragraph.
 
-# Note: Your game will have to accept user input so, like the Mad Libs generator,
-# you won't be able to run it using Sublime's `Build` feature.
-# Instead you'll need to run the program in Terminal or IDLE.
-# Refer to Work Session 5 if you need a refresher on how to do this.
-
-# To help you get started, we've provided a sample paragraph that you can use when testing your code.
-# Your game should consist of 3 or more levels, so you should add your own paragraphs as well!
-
-
-
-# The answer for ___1___ is 'function'. Can you figure out the others?
-
-# We've also given you a file called fill-in-the-blanks.pyc which is a working version of the project.
-# A .pyc file is a Python file that has been translated into "byte code".
-# This means the code will run the same as the original .py file, but when you open it
-# it won't look like Python code! But you can run it just like a regular Python file
-# to see how your code should behave.
-
-# Hint: It might help to think about how this project relates to the Mad Libs generator you built with Sean.
-# In the Mad Libs generator, you take a paragraph and replace all instances of NOUN and VERB.
-# How can you adapt that design to work with numbered blanks?
-
-# blanks = ['___1___','___2___','___3___','___4___']
-# correct_answers = ["Function","Argument","Zero","Array"]
-# user_answers = []
-# blanks_indices = []
-
-# paragraph_split = paragraph.split()
-# print(str(paragraph_split))
-
-
-
-#=========================================================
-#==== Build up the complete sentence =====================
-#=========================================================
-
-# def create_complete_sentence(user_answers,paragraph_split):
-# 	complete_paragraph = []
-	
-# 	for part in paragraph_split:
-# 		answer = get_blanks_indices(paragraph_split)
-# 		# print(answer)
-# 		if answer != None:
-# 			print("inside if condition")
-# 			part = part.replace(answer,user_answers[0])
-# 			print(part)
-# 			complete_paragraph.append(part)
-
-# 		else:
-# 			complete_paragraph.append(part)
-# 	return complete_paragraph
-
-#=========================================================
-#==== Get the indexes of blanks ==========================
-#=========================================================
-
-# blanks_indices = get_blanks_indices(paragraph_split)
-# for blank in range(0,4):
-# 	word = input("Fill in the blank #"+ str(blank+1) + ": ")
-# 	user_answers.append(word)
-# print("Your answers are: ")
-# print(user_answers)
-
-# if user_answers == correct_answers:
-# 	print("Answers are correct!")
-# 	print("The full sentence is:")
-# else:
-# 	print("One or more answers are wrong ... please try again")
-
-# complete_paragraph = create_complete_sentence(user_answers,paragraph_split)
-# print(complete_paragraph)
-# print(paragraph_split)
-# for index, part in enumerate(paragraph_split):
-# 	# print("inside the for loop")
-# 	if index in blanks_indices:
-		
-		# part = part.replace(part, "user_answers[index]")
-	# print(paragraph_split)
-
 #=========================================================
 #==== Select difficulty level ============================
 #=========================================================
@@ -91,25 +12,31 @@ def select_difficulty_level():
 	print("Your selected difficulty level is: " + str(user_level))
 
 	if user_level == "easy":
-		quiz_paragraph = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
-		adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
-		don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
-		tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
-		quiz_answers = ["a","s","d","f"]
+
+		quiz_paragraph = '''When I first brought my cat ___1___ from the Humane Society she was a mangy, pitiful 
+		___2___. She was so thin that you could count her vertebrae just by looking at her. Apparently ___4___ was 
+		declawed by her previous owners, then abandoned or ___3___. Since she couldn't hunt, ___4___ nearly starved. 
+		Not only that, but ___4___ had an abscess on one hip.'''
+
+		quiz_answers = ["home","animal","lost","she"]
 
 	elif user_level == "medium":
+
 		quiz_paragraph = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
 		adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
 		don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
 		tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
-		quiz_answers = ["Function","Argument","Zero","Array"]
+		
+		quiz_answers = ["function","argument","zero","array"]
 
 	elif user_level == "hard":
-		quiz_paragraph = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
-		adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
-		don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
-		tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
-		quiz_answers = ["Function","Argument","Zero","Array"]
+
+		quiz_paragraph = '''The longer the ___1___ or the more complex the criteria, the longer students will take 
+		to complete a thorough peer ___2___. If you assign shorter papers, you can easily devote a part of a ___3___
+		 to peer ___2___ or ask students to complete the peer ___2___ outside of ___3___. But if you assign long, 
+		 complex papers, consider breaking the peer ___2___ into several ___4___ sections. '''
+		
+		quiz_answers = ["paper","review","class","short"]
 
 	else:
 		print("Not a valid selection. Please try again.")
